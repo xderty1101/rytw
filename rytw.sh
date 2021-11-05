d7a2d666f0820fa233549b20107660efccd9c957
xderty1101 <58272067+xderty1101@users.noreply.github.com>
2021-11-05 15:53:56 +1300

    the bigger boi
    i like cows btw

--- rytw.sh
+++ rytw.sh
@@ -1,31 +1,74 @@
 #!/bin/bash
 
-echo \(1\)Play \| \(2\)Download
-
+echo pick resolution\/audio format thing? \(y\/N\)
 read userChoice
-
-if [ "$userChoice" = "1" ] || [ "$userChoice" = "P" ] || [ "$userChoice" = "p" ]
+if [ "$userChoice" = "Y" ] || [ "$userChoice" = "y" ]
 then
-	echo -e "you chose Play\npls put 1 link in"
+	echo \(1\)Play \| \(2\)Download
 	read userChoice
-	yt-dlp -o - $userChoice | mpv -
-elif [ "$userChoice" = "2" ] || [ "$userChoice" = "D" ] || [ "$userChoice" = "d" ]
+	if [ "$userChoice" = "1" ] || [ "$userChoice" = "P" ] || [ "$userChoice" = "p" ]
+	then
+		echo -e "you chose Play\npls put 1 link in"
+		read userChoice
+		yt-dlp -F $userChoice
+		echo -e "\n pick video+audio or smth idk, space with + eg. 123+456"
+		read userChoiceF
+		yt-dlp -f $userChoiceF -o - $userChoice | mpv -
+	elif [ "$userChoice" = "2" ] || [ "$userChoice" = "D" ] || [ "$userChoice" = "d" ]
+	then
+		echo -e "you chose Download\n(1)Audio | (2)Video"
+		read userChoice
+		if [ "$userChoice" = "1" ] || [ "$userChoice" = "A" ] || [ "$userChoice" = "a" ]
+		then
+			echo -e "put in 1 or more links\nSeparate with spaces"
+			read userChoice
+			yt-dlp -F $userChoice
+			echo -e "\n pick video+audio or smth idk, space with + eg. 123+456"
+			read userChoiceF
+			yt-dlp -f $userChoiceF -x $userChoice
+		elif [ "$userChoice" = "2" ] || [ "$userChoice" = "V" ] || [ "$userChoice" = "v" ]
+		then
+			echo -e "put in 1 or more links\nSeparate with spaces"
+			read userChoice
+			yt-dlp -F $userChoice
+			echo -e "\n pick video+audio or smth idk, space with + eg. 123+456"
+			read userChoiceF
+			yt-dlp -f $userChoiceF $userChoice
+		else
+			echo dumb fuck put 1 or 2
+		fi
+	else
+		echo dumb fuck put 1 or 2
+	fi
+elif [ "$userChoice" = "N" ] || [ "$userChoice" = "n" ] || [ "$userChoice" = "" ]
 then
-	echo -e "you chose Download\n(1)Audio | (2)Video"
+	echo \(1\)Play \| \(2\)Download
 	read userChoice
-	if [ "$userChoice" = "1" ] || [ "$userChoice" = "A" ] || [ "$userChoice" = "a" ]
+	if [ "$userChoice" = "1" ] || [ "$userChoice" = "P" ] || [ "$userChoice" = "p" ]
 	then
-		echo -e "put in 1 or more links\nSeparate with spaces"
+		echo -e "you chose Play\npls put 1 link in"
 		read userChoice
-		yt-dlp -x --audio-format mp3 $userChoice
-	elif [ "$userChoice" = "2" ] || [ "$userChoice" = "V" ] || [ "$userChoice" = "v" ]
+		yt-dlp -o - $userChoice | mpv -
+	elif [ "$userChoice" = "2" ] || [ "$userChoice" = "D" ] || [ "$userChoice" = "d" ]
 	then
-		echo -e "put in 1 or more links\nSeparate with spaces"
+		echo -e "you chose Download\n(1)Audio | (2)Video"
 		read userChoice
-		yt-dlp -f mp4 $userChoice
+		if [ "$userChoice" = "1" ] || [ "$userChoice" = "A" ] || [ "$userChoice" = "a" ]
+		then
+			echo -e "put in 1 or more links\nSeparate with spaces"
+			read userChoice
+			yt-dlp -x --audio-format mp3 $userChoice
+		elif [ "$userChoice" = "2" ] || [ "$userChoice" = "V" ] || [ "$userChoice" = "v" ]
+		then
+			echo -e "put in 1 or more links\nSeparate with spaces"
+			read userChoice
+			yt-dlp -f mp4 $userChoice
+		else
+			echo dumb fuck put 1 or 2
+		fi
 	else
 		echo dumb fuck put 1 or 2
 	fi
 else
-	echo dumb fuck put 1 or 2
+	echo fuck off
 fi
